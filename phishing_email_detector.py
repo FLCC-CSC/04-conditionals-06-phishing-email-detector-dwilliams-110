eca96908f63ca79ad842dc74d613f71f10c3b212
Daniel Williams <dwilliams110@fingerlakes.edu>
2026-03-31 03:51:26 +0000

    using \'in\' and \'or\' operators

--- phishing_email_detector.py
+++ phishing_email_detector.py
@@ -1,8 +1,8 @@
 # FILE NAME - phishing_email_detector.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Daniel Williams
+# DATE: 3/38/3036
+# BRIEF DESCRIPTION: Using \'in\' and \'or\' operators 
 
 
 
@@ -15,12 +15,32 @@
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+subject_line = input('Enter the email subject line: ')
 
-
-
-
-
-
+if 'urgent' in subject_line.lower():
+    print()
+    print('SECURITY ASSESSMENT:')
+    print('HIGH RISK: Possible phishing attempt.')
+    print('------------------------')
+    print(f'Analyzed subject: \"{subject_line}\"')
+elif 'win' in subject_line.lower() or 'free' in subject_line.lower():
+    print()
+    print('SECURITY ASSESSMENT:')
+    print('MEDIUM RISK: Suspicious offer detected.')
+    print('------------------------')
+    print(f'Analyzed subject: \"{subject_line}\"')
+elif 'password reset' in subject_line.lower():
+    print()
+    print('SECURITY ASSESSMENT:')
+    print('LOW RISK: Verify legitimacy with sender.')
+    print('------------------------')
+    print(f'Analyzed subject: \"{subject_line}\"')
+else:
+    print()
+    print('SECURITY ASSESSMENT:')
+    print('No phishing indicators detected.')
+    print('------------------------')
+    print(f'Analyzed subject: \"{subject_line}\"')
 
 
 
@@ -76,7 +96,7 @@ Analyzed subject: "Did you request a password reset?"
 
 '''
 
-1. Was using `in` difficult or was it natural?
+1. Was using `in` difficult or was it natural? Using 'in' was very nice and natural. Using the or was a bit tricky, I didn't think you'd have to put the string variable twice. I had to google this.
 
 
 
@@ -97,7 +117,7 @@ Analyzed subject: "Did you request a password reset?"
 Please gauge your utilization of AI on the following spectrum. Place an "X" in front
 of the appropriate response. Only choose one of the following:
 
-[ ] I did not use AI at all for this lab.
+[X] I did not use AI at all for this lab.
 [ ] I wrote the initial draft of the software but had AI help me make it better.
 [ ] I fed the lab description to AI and had it generate a response but I modified it.
 [ ] AI created the entire program for me.
@@ -112,6 +132,6 @@ may impede your understanding. Please rate how well you understand the concepts 
 [ ] I understand very little about this lab.
 [ ] I am about 50/50 on this lab; I get parts of it but not the whole picture.
 [ ] I pretty much get it.
-[ ] I'm solid. Totally got it.
+[X] I'm solid. Totally got it.
 
 '''
